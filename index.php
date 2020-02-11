3a9a6b31b7b63585cc240395a62cbe67bafed68e
dwinanapitupulu <52696140+dwinanapitupulu@users.noreply.github.com>
2020-02-11 09:30:38 +0700

    v1
    v1

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php require_once("head.php");?>
+  
+  <body>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+
+    <?php require_once("header.php");?>
+
+  <div class="container-fluid" data-aos="fade" data-aos-delay="500">
+    <div class="swiper-container images-carousel">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <div class="image-wrap">
+                <div class="image-info">
+                  <h2 class="mb-3">CASE</h2>
+                  <a href="customcase.php" class="btn btn-outline-white py-2 px-4">More Photos</a>
+                </div>
+                <img src="images/img_01a.jpg" alt="Image">
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="image-wrap">
+                <div class="image-info">
+                  <h2 class="mb-3">MUG</h2>
+                  <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
+                </div>
+                <img src="images/img_02.jpg" alt="Image">
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="image-wrap">
+                <div class="image-info">
+                  <h2 class="mb-3">THUMBLER</h2>
+                  <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
+                </div>
+                <img src="images/img_03.jpg" alt="Image">
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="image-wrap">
+                <div class="image-info">
+                  <h2 class="mb-3">KAOS</h2>
+                  <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
+                </div>
+                <img src="images/img_04.png" alt="Image">
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="image-wrap">
+                <div class="image-info">
+                  <h2 class="mb-3">FLASHDISC</h2>
+                  <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
+                </div>
+                <img src="images/img_05.png" alt="Image">
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="image-wrap">
+                <div class="image-info">
+                  <h2 class="mb-3">Power Bank</h2>
+                  <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
+                </div>
+                <img src="images/img_06.png" alt="Image">
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="image-wrap">
+                <div class="image-info">
+                  <h2 class="mb-3">Pop Socket</h2>
+                  <a href="single.html" class="btn btn-outline-white py-2 px-4">More Photos</a>
+                </div>
+                <img src="images/img_07.jpg" alt="Image">
+              </div>
+            </div>
+        </div>
+
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-scrollbar"></div>
+    </div>
+  </div>
+
+  
+
+    
+
+    
+    
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/swiper.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/picturefill.min.js"></script>
+  <script src="js/lightgallery-all.min.js"></script>
+  <script src="js/jquery.mousewheel.min.js"></script>
+
+  <script src="js/main.js"></script>
+  
+  <script>
+    $(document).ready(function(){
+      $('#lightgallery').lightGallery();
+    });
+  </script>
+    
+  </body>
+</html>
